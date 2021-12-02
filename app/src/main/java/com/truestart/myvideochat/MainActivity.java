eda7bf7f73a5698dcf4cfebd12e7439e68ac82b4
Antoni Vasilev <antonivasilev15@gmail.com>
2021-12-02 18:13:56 +0200

    Save and Load last room opened

--- app/src/main/java/com/truestart/myvideochat/MainActivity.java
+++ app/src/main/java/com/truestart/myvideochat/MainActivity.java
@@ -2,6 +2,8 @@ package com.truestart.myvideochat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,20 +28,35 @@ public class MainActivity extends AppCompatActivity {
         join = findViewById(R.id.join);
         groupName = findViewById(R.id.roomName);
         join.setOnClickListener(v -> createOrJoinRoom(groupName.getText().toString().trim()));
+        loadRoomName();
+    }
+
+    private void loadRoomName() {
+        SharedPreferences sharedPreferences = getSharedPreferences("App", MODE_PRIVATE);
+        groupName.setText(sharedPreferences.getString("roomName", null));
     }
 
     private void createOrJoinRoom(String roomName) {
+        saveRoomName(roomName);
         try {
             JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
                     .setServerURL(new URL("https://meet.jit.si"))
                     .setRoom(roomName)
-                    .setAudioMuted(false)
-                    .setVideoMuted(false)
+                    .setWelcomePageEnabled(true)
+                    .setAudioMuted(true)
+                    .setVideoMuted(true)
                     .setWelcomePageEnabled(false)
                     .build();
             JitsiMeetActivity.launch(this, options);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private void saveRoomName(String roomName) {
+        SharedPreferences.Editor sharedPreferences = getSharedPreferences("App", MODE_PRIVATE).edit();
+        sharedPreferences.putString("roomName", roomName);
+        sharedPreferences.apply();
     }
 }
